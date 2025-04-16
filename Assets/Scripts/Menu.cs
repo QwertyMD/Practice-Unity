23c5b8b3e9a27f4dc780ce2fae96a909e21b8c94
QwertyMD <aaryandhakal2020@gmail.com>
2025-04-16 13:12:33 +0545

    add door open/close, parking lot

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [Header("UI Panels")]
+    public GameObject mainPanel;
+    public GameObject aboutPanel;
+    public GameObject creditsPanel;
+
+    void Start()
+    {
+        ShowMainMenu();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        SceneManager.LoadScene("Zoo"); // Replace with your Zoo scene name
+    }
+
+    public void ShowAbout()
+    {
+        mainPanel.SetActive(false);
+        aboutPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        mainPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainPanel.SetActive(true);
+        aboutPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
